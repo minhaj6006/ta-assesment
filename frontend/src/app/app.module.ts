86afe0a31da9e8d057a1016e0cc3fcc1da0f29a2
Minhaj ur Rehman <72701898+minhaj6006@users.noreply.github.com>
2025-03-01 03:19:58 +0500

    Update global styles and enhance Angular Material module imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,8 +4,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { ConversionComponent } from './components/conversion/conversion.component';
@@ -25,7 +30,12 @@ import { ThousandSeparatorDirective } from './directives/thousand-separator.dire
     HttpClientModule,
     BrowserAnimationsModule,
     MatProgressSpinnerModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
