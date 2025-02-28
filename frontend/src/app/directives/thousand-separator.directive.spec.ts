0de42437729f762e489e7e2503d0898b9eacf482
Minhaj ur Rehman <72701898+minhaj6006@users.noreply.github.com>
2025-03-01 03:36:36 +0500

    Add ThousandSeparatorDirective for formatting input values

---
+++ frontend/src/app/directives/thousand-separator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ThousandSeparatorDirective } from './thousand-separator.directive';
+
+describe('ThousandSeparatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ThousandSeparatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
